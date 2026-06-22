d16c2f4e6eb126b135f0d51da33b953fea49e010
SlimDaddy <>
2026-06-22 14:10:21 +0100

    new

--- tests/BrowserContext.spec.js
+++ tests/BrowserContext.spec.js
@@ -2,14 +2,15 @@ import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://demo.learnwebdriverio.com/login');
-    await page.getByLabel('Email').fill('newtestuser4@test.com');
-    await page.getByLabel('Password').fill('pwd');
+    //await page.pause()
+    await page.getByRole('textbox', { name: 'Email' }).fill('newtestuser4@test.com');
+    await page.getByRole('textbox',{ name: 'password' }).fill('pwd');
     await page.getByRole('button', { name: 'Sign in' }).click();
 })
 
 
 
-test('test with BrowserContext', async () => {
+test('test with BrowserContext', async ({ page }) => {
 
     await expect(page.getByRole('link', { name: 'olegqa3' })).toBeVisible();
 
